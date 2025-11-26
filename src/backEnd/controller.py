0e4ba671fb3e468a2fa2cc2306843d7d1442a164
Ricardo Prado Faria <ricardofaria170@gmail.com>
2025-11-26 19:29:54 -0300

    ADD Payment and Reserve logics

--- src/backEnd/controller.py
+++ src/backEnd/controller.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from src.backEnd.db import get_connection 
 
 app = FastAPI()
 
